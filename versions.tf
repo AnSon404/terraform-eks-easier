3e0d556649b37c9081ac8ca800a1085e60964f61
AnSon404 <pps023002_2005@yahoo.com.hk>
2023-06-25 00:18:50 +0800

    Added my project

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
-terraform {
-  required_version = ">= 1.0"
+# terraform {
+#   required_version = ">= 1.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.47"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 4.47"
+#     }
+#   }
 
   # ##  Used for end-to-end testing on project; update to suit your needs
   # backend "s3" {
